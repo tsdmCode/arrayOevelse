a5555556c4051d61f8aa180c45b4246b80218fb8
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-03-19 10:06:18 +0100

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,27 @@
-console.log("Dø i helvede");
+console.log('Dø i helvede');
+
+let arr = [
+  'Diller',
+  'penis',
+  'Javertus',
+  'PIK',
+  'tissemand',
+  'sjover',
+  'lem',
+  'kæp',
+  'spyd',
+  'stang',
+  'jern',
+  'kødfløjte',
+  'big poppa',
+];
+
+arr.forEach((i) => {
+  const list = document.getElementById('piklist');
+  const li = document.createElement('li');
+  li.innerText = i;
+
+  console.log(li);
+
+  list.appendChild(li);
+});
